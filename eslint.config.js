02a69c8058bfe3b47184d63164226d2cecb0f3cf
hminaee-tc <hminaee@tucows.com>
2024-07-19 11:21:41 -0300

    feat/ICE-989-improvement-2 add linting

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+
+  {
+   languageOptions: {
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn", // Warns about variables that are declared but not used
+      "no-undef": "warn", // Warns about the use of undeclared variables
+      eqeqeq: "error", // Requires the use of === and !== instead of == and !=
+      curly: "error", // Requires curly brace conventions for all control statements
+      "no-redeclare": "error", // Disallows redeclaring variables
+      "no-console": "warn", // Warns about the use of console (useful in production code)
+      "no-debugger": "error", // Disallows the use of debugger
+      "no-trailing-spaces": "error", // Disallows trailing whitespace at the end of lines
+      "no-multi-spaces": "error", // Disallows multiple spaces (except for indentation)
+      "no-unused-expressions": "warn", // Warns about unused expressions which have no effect
+      "prefer-const": "warn", // Suggests using const declaration for variables that are never reassigned after declared
+      "arrow-spacing": ["error", { before: true, after: true }], // Enforces consistent spacing before and after the arrow in arrow functions
+      indent: ["error", 2], // Enforces a consistent indentation style (2 spaces in this example)
+      quotes: ["error", "single"], // Enforces the use of single quotes for strings
+      semi: ["error", "always"], // Requires semicolons at the end of statements
+    },
+  },
+];
