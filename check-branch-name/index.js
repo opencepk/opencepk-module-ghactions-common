00c695cf177f9fc516804c13e64d0346c7a3a68c
hminaee-tc <hminaee@tucows.com>
2024-07-30 10:46:14 -0300

    feat/commitmessage branch name check

--- check-branch-name/index.js
+++ check-branch-name/index.js
@@ -2,9 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // Get the branch name from the GitHub context
-  const branchName = github.context.ref.replace('refs/heads/', '');
+  // Get the pull request event payload
+  const pullRequest = github.context.payload.pull_request;
 
+  // Extract the branch name from the pull request event
+  const branchName = pullRequest.head.ref;
+  core.info(`Branch name: ${branchName}`);
   // Define the branch name pattern
   const branchPattern =
     /^(feat|fix|build|breaking|chore|ci|docs|perf|refactor|revert|test)\/[a-zA-Z0-9-]+$/;
