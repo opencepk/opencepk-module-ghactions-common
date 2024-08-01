52ac5de4481d321270ce0a23646819746fb51854
hminaee-tc <hminaee@tucows.com>
2024-08-01 11:52:13 -0300

    feat/commitmessage testing logging

--- common/logger.js
+++ common/logger.js
@@ -1,22 +1,28 @@
-// import * as core from '@actions/core';
 const core = require('@actions/core');
 const red = '\x1b[31m';
 const blue = '\x1b[34m';
 const green = '\x1b[32m';
 const reset = '\x1b[0m';
 
-export function error(message) {
+function error(message) {
   core.error(`${red}${message}${reset}`);
 }
 
-export function info(message) {
+function info(message) {
   core.info(`${blue}${message}${reset}`);
 }
 
-export function debug(message) {
+function debug(message) {
   core.debug(`${green}${message}${reset}`);
 }
 
-export function setFailed(message) {
+function setFailed(message) {
   core.setFailed(`${red}${message}${reset}`);
 }
+
+module.exports = {
+  error,
+  info,
+  debug,
+  setFailed
+};
--- github-flow-check-branch-name/index.js
+++ github-flow-check-branch-name/index.js
@@ -1,28 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const logger = require('../dist');
 
 // import * as github from '@actions/github';
 // import * as logger from '../dist/logger.js';
 
 try {
+  logger.debug('xxxxxxxxxxxx    Checking branch name...');
   // Get the pull request event payload
   const pullRequest = github.context.payload.pull_request;
 
   // Extract the branch name from the pull request event
   const branchName = pullRequest.head.ref;
-  core.info(`Branch name: ${branchName}`);
+  logger.info(`Branch name: ${branchName}`);
   // Define the branch name pattern
   const branchPattern =
     /^(feat|fix|build|breaking|chore|ci|docs|perf|refactor|revert|test)\/[a-zA-Z0-9-]+$/;
 
   // Check if the branch name matches the pattern
   if (!branchPattern.test(branchName)) {
-    core.setFailed(
+    logger.setFailed(
       `Branch name "${branchName}" does not follow the required format such as feat/ice-123 ({type}/{issue_number}). Please rename the branch using git branch -m ${branchName} <newname>`,
     );
   } else {
-    core.info(`Branch name "${branchName}" is valid.`);
+    logger.info(`Branch name "${branchName}" is valid.`);
   }
 } catch (error) {
-  core.setFailed(error.message);
+  logger.setFailed(error.message);
 }
