c5fc286d35f47c33a66a143c50b1461f22734d0a
hminaee-tc <hminaee@tucows.com>
2024-08-06 18:56:35 -0300

    feat/sync-with-mirror sync with mirror

--- common/logger.js
+++ common/logger.js
@@ -1,9 +1,8 @@
 const core = require('@actions/core');
-const chalk = require('chalk');
+
 
 const red = '\x1b[31m';
 const blue = '\x1b[34m';
-// const blue = styles.color.ansi16m(...styles.hexToRgb('#abcdef'));
 const green = '\x1b[32m';
 const reset = '\x1b[0m';
 
@@ -11,9 +10,8 @@ function error(message) {
   core.error(`${red}${message}${reset}`);
 }
 
-function info(message, color = '#FF0000') {
-  // core.info(`${blue}${message}${reset}`);
-  core.error(chalk.hex(color)(message));
+function info(message) {
+  core.info(`${blue}${message}${reset}`);
 }
 
 function debug(message) {
