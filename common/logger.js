f1cc4f3bb6841e8631d003f9fa2e5b706aa83f60
hminaee-tc <hminaee@tucows.com>
2024-08-06 18:48:53 -0300

    feat/sync-with-mirror sync with mirror

--- common/logger.js
+++ common/logger.js
@@ -1,15 +1,15 @@
 const core = require('@actions/core');
-const styles = require('ansi-styles');
-console.log('Styles:', styles); // Debug statement to check the import
-console.log('zxxxxx:', JSON.stringify(styles)); // Debug statement to check the import
+const chalk = require('chalk');
+
 const red = '\x1b[31m';
-// const blue = '\x1b[34m';
-const blue = styles.color.ansi16m(...styles.hexToRgb('#abcdef'));
+const blue = '\x1b[34m';
+// const blue = styles.color.ansi16m(...styles.hexToRgb('#abcdef'));
 const green = '\x1b[32m';
 const reset = '\x1b[0m';
 
 function error(message) {
-  core.error(`${red}${message}${reset}`);
+  core.error(chalk.hex(blue)(message));
+  // core.error(`${red}${message}${reset}`);
 }
 
 function info(message) {
