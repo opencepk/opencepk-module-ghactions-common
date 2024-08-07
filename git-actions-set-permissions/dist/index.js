8cf89e6b7eeabe3dd8987ed634a5c20ecd5e72f8
hminaee-tc <hminaee-tc@users.noreply.github.com>
2024-08-07 19:01:37 +0000

    chores/update-dist: Build dist folders

--- git-actions-set-permissions/dist/index.js
+++ git-actions-set-permissions/dist/index.js
@@ -61047,6 +61047,7 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const logger = __nccwpck_require__(5568);
 
+
 async function run() {
   try {
     const token = core.getInput('token');
