9e9f87c10d690b256fad98d440c43256dff46b49
hminaee-tc <hminaee-tc@users.noreply.github.com>
2024-08-07 04:01:37 +0000

    chores/update-dist: Build dist folders

--- github-app-token/dist/index.js
+++ github-app-token/dist/index.js
@@ -1,7 +1,7 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 4237:
+/***/ 1655:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const jwt = __nccwpck_require__(7486);
@@ -145,6 +145,16 @@ function validateInput(name, value) {
   return value.trim();
 }
 
+module.exports = GitHubApplication;
+
+
+/***/ }),
+
+/***/ 4237:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const GitHubApplication = __nccwpck_require__(1655);
+
 module.exports = {
   create: (privateKey, applicationId, baseApiUrl, timeout, proxy) => {
     const app = new GitHubApplication(privateKey, applicationId, baseApiUrl);
@@ -163,6 +173,52 @@ module.exports = {
     }
   },
 };
+
+function getOctokit(token, baseApiUrl, proxy) {
+  const octokitOptions = {
+    baseUrl: baseApiUrl,
+    request: {
+      agent: getProxyAgent(proxy, baseApiUrl),
+      timeout: 5000,
+    },
+  };
+  return github.getOctokit(token, octokitOptions);
+}
+
+function getProxyAgent(proxy, baseUrl) {
+  if (proxy) {
+    core.info(`explicit proxy specified as '${proxy}'`);
+    return new HttpsProxyAgent(proxy);
+  }
+
+  const envProxy =
+    process.env.http_proxy ||
+    process.env.HTTP_PROXY ||
+    process.env.https_proxy ||
+    process.env.HTTPS_PROXY;
+  if (envProxy) {
+    core.info(`environment proxy specified as '${envProxy}'`);
+    const noProxy = process.env.no_proxy || process.env.NO_PROXY;
+    if (noProxy && !proxyExcluded(noProxy, baseUrl)) {
+      core.info(`using proxy '${envProxy}' for GitHub API calls`);
+      return new HttpsProxyAgent(envProxy);
+    }
+  }
+  return null;
+}
+
+function proxyExcluded(noProxy, baseUrl) {
+  const noProxyHosts = noProxy.split(',').map(part => part.trim());
+  const baseUrlHost = new URL.URL(baseUrl).host;
+  return noProxyHosts.includes(baseUrlHost);
+}
+
+function validateInput(name, value) {
+  if (!value || !value.trim()) {
+    throw new Error(`A valid ${name} must be provided, was "${value}"`);
+  }
+  return value.trim();
+}
 
 
 /***/ }),
