73f241f04c93de3ea4402985b408ba605ee8968d
hminaee-tc <hminaee@tucows.com>
2024-08-13 18:25:33 -0300

    chores/fix-sources cleanup

--- github-flow-mirror-repo/index.js
+++ github-flow-mirror-repo/index.js
@@ -43,61 +43,14 @@ async function processRepo(publicRepoUrl, org, token, newRepoName = null) {
   );
   execSync('git config user.name "github-actions[bot]"');
 
-  // Add UPSTREAM file
-  logger.info('Adding UPSTREAM file');
-  const upstreamContent = `git@github.com:${
-    publicRepoUrl.split('https://github.com/')[1]
-  }.git`;
-  const upstreamFilePath = path.join('.github', 'UPSTREAM');
-  fs.mkdirSync(path.dirname(upstreamFilePath), { recursive: true });
-  fs.writeFileSync(upstreamFilePath, upstreamContent);
-
-  // Commit the UPSTREAM file
-  logger.info('Committing UPSTREAM file');
-  execSync('git add .github/UPSTREAM');
-  execSync('git commit -m "Add UPSTREAM file"');
-
-  // Add the GitHub Actions workflow file
-  logger.info('Adding GitHub Actions workflow file');
-  const workflowContent = `
----
-name: sync-with-mirror
-on:
-  schedule:
-    - cron: '*/10 * * * *'
-  workflow_dispatch:
-jobs:
-  sync:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v4
-      - name: Setup SSH Agent
-        uses: webfactory/ssh-agent@v0.9.0
-        with:
-          ssh-private-key: \${{ secrets.SSH_KEY_ICE_MODULES_READONLY }}
-      - name: Sync with Upstream
-        uses: ${org}/opencepk-module-ghactions-common/github-flow-sync-with-mirror@main
-        with:
-          github_token: \${{ secrets.GITHUB_TOKEN }}
-    `;
-  const workflowFilePath = path.join(
-    '.github',
-    'workflows',
-    'sync-with-mirror.yml',
-  );
-  fs.mkdirSync(path.dirname(workflowFilePath), { recursive: true });
-  fs.writeFileSync(workflowFilePath, workflowContent);
-
-  // Commit the workflow file
-  logger.info('Committing workflow file');
-  execSync('git add .github/workflows/sync-with-mirror.yml');
-  execSync('git commit -m "Add sync-with-mirror workflow"');
-
-  // Set the remote URL with the token for authentication
-  logger.info('Setting remote URL with token for authentication');
+  // Set the push location to the private repository
   const remoteUrl = `https://x-access-token:${token}@github.com/${org}/${repoName}.git`;
-  execSync(`git remote set-url origin ${remoteUrl}`);
+  execSync(`git remote set-url --push origin ${remoteUrl}`);
+
+  // Fetch updates from the original repository
+  execSync('git fetch -p origin');
+
+  // Push the updates to the private repository
   execSync('git push --mirror');
 
   core.setOutput('private_repo_url', privateRepo.html_url);
