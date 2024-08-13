b2ebd220b041046c40a76a66875d7902a1dd1775
hminaee-tc <hminaee@tucows.com>
2024-08-13 18:38:01 -0300

    chores/fix-sources cleanup

--- github-flow-mirror-repo/dist/index.js
+++ github-flow-mirror-repo/dist/index.js
@@ -65391,8 +65391,8 @@ async function processRepo(publicRepoUrl, org, token, newRepoName = null) {
     visibility: 'internal',
   });
 
-  // Clone the public repository
-  execSync(`git clone ${publicRepoUrl} public-repo`);
+  // Clone the public repository with all tags
+  execSync(`git clone --mirror ${publicRepoUrl} public-repo`);
   process.chdir('public-repo');
   logger.info('Configured Git user');
   // Configure Git user
@@ -65456,7 +65456,7 @@ jobs:
   logger.info('Setting remote URL with token for authentication');
   const remoteUrl = `https://x-access-token:${token}@github.com/${org}/${repoName}.git`;
   execSync(`git remote set-url origin ${remoteUrl}`);
-  execSync('git push origin main');
+  execSync('git push --mirror');
 
   core.setOutput('private_repo_url', privateRepo.html_url);
   const response = await setGitActionAccess(
@@ -65495,7 +65495,6 @@ async function run() {
 }
 
 run();
-
 })();
 
 module.exports = __webpack_exports__;
