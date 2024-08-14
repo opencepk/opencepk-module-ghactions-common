2372d7bcd05e219052d48f6acdc9163d2462657d
hminaee-tc <hminaee@tucows.com>
2024-08-14 19:34:06 -0300

    fix/ICE-1010-improvement js docs gh pages

--- common/logger.js
+++ common/logger.js
@@ -1,3 +1,6 @@
+/**
+ * @namespace logger
+ */
 const core = require('@actions/core');
 
 const red = '\x1b[31m';
@@ -5,18 +8,42 @@ const blue = '\x1b[34m';
 const green = '\x1b[32m';
 const reset = '\x1b[0m';
 
+/**
+ * Logs an error message.
+ *
+ * @memberof logger
+ * @param {string} message - The error message to log.
+ */
 function error(message) {
   core.error(`${red}${message}${reset}`);
 }
 
+/**
+ * Logs an informational message.
+ *
+ * @memberof logger
+ * @param {string} message - The informational message to log.
+ */
 function info(message) {
   core.info(`${blue}${message}${reset}`);
 }
 
+/**
+ * Logs a debug message.
+ *
+ * @memberof logger
+ * @param {string} message - The debug message to log.
+ */
 function debug(message) {
   core.debug(`${green}${message}${reset}`);
 }
 
+/**
+ * Sets the action as failed with a message.
+ *
+ * @memberof logger
+ * @param {string} message - The failure message to log.
+ */
 function setFailed(message) {
   core.setFailed(`${red}${message}${reset}`);
 }
