24026a2063dfa9731624aa3b1bd12ce64a9c13d1
hminaee-tc <hminaee-tc@users.noreply.github.com>
2024-08-19 17:27:38 +0000

    chores/update-dist: Build dist folders

--- github-flow-check-branch-name/dist/index.js
+++ github-flow-check-branch-name/dist/index.js
@@ -9,10 +9,15 @@ const core = __nccwpck_require__(4561);
 const red = '\x1b[31m';
 const blue = '\x1b[34m';
 const green = '\x1b[32m';
+const yellow = '\x1b[33m';
 const reset = '\x1b[0m';
 
 function error(message) {
   core.error(`${red}${message}${reset}`);
+}
+
+function warn(message) {
+  core.warning(`${yellow}${message}${reset}`);
 }
 
 function info(message) {
@@ -29,6 +34,7 @@ function setFailed(message) {
 
 module.exports = {
   error,
+  warn,
   info,
   debug,
   setFailed,
