2e2129fe1ba7d6311b7bf71acb3cd9b21329c198
hminaee-tc <hminaee-tc@users.noreply.github.com>
2024-08-21 15:41:40 +0000

    chores/update-dist: Build dist folders

--- github-set-topics/dist/index.js
+++ github-set-topics/dist/index.js
@@ -33822,6 +33822,7 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const fs = __nccwpck_require__(5630);
+
 const path = __nccwpck_require__(1017);
 
 async function run() {
