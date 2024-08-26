4350a8fe095fbc63758715ce0666bff6b31e36a9
hminaee-tc <hminaee@tucows.com>
2024-08-26 13:10:38 -0300

    fix/update-gitmodules-action test

--- github-flow-sync-with-mirror/index.js
+++ github-flow-sync-with-mirror/index.js
@@ -201,19 +201,12 @@ async function run() {
       `upstream/${branch}`,
     ]);
 
-    // // Push the merge branch to origin
-    // await exec.exec('git', ['push', 'origin', mergeBranch]);
-
-    // Temporarily set the remote URL to use HTTPS with the token
-    const originalRemoteUrl = `https://github.com/${owner}/${repo}.git`;
-    const remoteUrlWithToken = `https://${token}@github.com/${owner}/${repo}.git`;
-    await exec.exec('git', ['remote', 'set-url', 'origin', remoteUrlWithToken]);
+    // Set the remote URL to use HTTPS with the token
+    const remoteUrl = `https://${token}@github.com/${owner}/${repo}.git`;
+    await exec.exec('git', ['remote', 'set-url', 'origin', remoteUrl]);
 
     // Push the merge branch to origin
     await exec.exec('git', ['push', 'origin', mergeBranch]);
-
-    // Reset the remote URL back to the original
-    await exec.exec('git', ['remote', 'set-url', 'origin', originalRemoteUrl]);
 
     // Create a pull request
     await octokit.pulls.create({
