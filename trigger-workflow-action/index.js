eced8f3a854953c3e305da2fe4e4311e13c1f954
hminaee-tc <hminaee@tucows.com>
2024-08-26 21:54:05 -0300

    fix/update-gitmodules-action test

--- trigger-workflow-action/index.js
+++ trigger-workflow-action/index.js
@@ -1,22 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const logger = require('../common/logger');
 
 async function run() {
   try {
     const token = core.getInput('token');
     const repo = core.getInput('repo');
+    logger.debug(`Received repo input: ${repo}`);
     const workflow_id = core.getInput('workflow_id');
+    logger.debug(`Received workflow_id input: ${workflow_id}`);
     const ref = core.getInput('ref');
+    logger.debug(`Received ref input: ${ref}`);
     const inputs = core.getInput('inputs');
-
+    logger.debug(`Received inputs input: ${inputs}`);
     const [owner, repoName] = repo.split('/');
     if (!owner || !repoName) {
       core.setFailed('Invalid repo format. Expected format: owner/repo');
       return;
     }
-
+    logger.info(`Repository owner: ${owner} Repository name: ${repoName}`);
     const octokit = github.getOctokit(token);
-
+    logger.info(`Triggering workflow ${workflow_id} on ${repo}`);
     await octokit.rest.actions.createWorkflowDispatch({
       owner,
       repo: repoName,
@@ -24,9 +28,9 @@ async function run() {
       ref,
       inputs: JSON.parse(inputs),
     });
-
     core.info(`Successfully triggered workflow ${workflow_id} on ${repo}`);
   } catch (error) {
+    logger.error(JSON.stringify(error));
     core.setFailed(error.message);
   }
 }
